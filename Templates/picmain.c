33a70dbae3219a99f32fc14b9c5befcbc87efe6c
Gavin lyons <glyons66@hotmail.com>
2020-09-12 16:59:05 +0100

    updating templates

---
+++ Templates/picmain.c
@@ -0,0 +1,47 @@
+/* -------- Header -------- */
+/*
+* File: main.c
+* Description: 
+* Author: Gavin Lyons.
+* Complier: xc8 v2.10 compiler
+* PIC: 
+* IDE:  MPLAB X v5.30
+* Created
+* Description: See URL for full details.
+* URL: 
+*/
+
+/* -------- libraries -------- */
+#include "mcc_generated_files/mcc.h"
+
+
+/*---------------- Globals---------*/
+
+   
+/* ----------- Defines -----------*/
+#define INIT_DELAY 1000
+
+/* -------- Function prototypes -------- */
+void Setup(void);
+
+/* --------- Main Loop ---------- */
+void main(void)
+{ 
+    Setup();
+    while (1)
+    {
+		__nop();
+    }
+}
+/* --------- End of Main ---------- */
+
+
+/* ------------ Function Space -------- */
+
+// Function System Setup
+void Setup(void)
+{
+    SYSTEM_Initialize();
+}
+
+/* End of File */
